39d7da4eb25496a86f950fbdf404c637e5f87d8d
Panit Tuangsuwan <tpanitte@hotmail.com>
2020-08-15 14:43:29 +0700

    v0.0.8

--- packages/supervalidatorsjs/src/index.ts
+++ packages/supervalidatorsjs/src/index.ts
@@ -1,15 +1,18 @@
+import { either } from "./either";
 import { inEmailFormat } from "./inEmailFormat";
 import { inStringLength } from "./inStringLength";
 import { isRequired } from "./isRequired";
 import { typeOf } from "./typeOf";
 
 export { adapters } from "./adapters";
+export { either, eitherRules } from "./either";
 export { inEmailFormat, inEmailFormatRules } from "./inEmailFormat";
 export { inStringLength, inStringLengthRules } from "./inStringLength";
 export { isRequired, isRequiredRules } from "./isRequired";
 export { typeOf, typeOfRules } from "./typeOf";
 
 export const Validators = {
+  either,
   inEmailFormat,
   inStringLength,
   isRequired,
