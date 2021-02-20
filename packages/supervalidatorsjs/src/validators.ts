9bce1312234f9ecce71b43043e8593fb1e689f7f
Panit Tuangsuwan <tpanitte@hotmail.com>
2021-02-20 18:07:42 +0700

    feat: update validators

--- packages/supervalidatorsjs/src/validators.ts
+++ packages/supervalidatorsjs/src/validators.ts
@@ -1,10 +1,16 @@
+import { either } from "./either";
 import { inEmailFormat } from "./inEmailFormat";
+import { inStringLength } from "./inStringLength";
 import { isRequired } from "./isRequired";
+import { oneOf } from "./oneOf";
 import { typeOf } from "./typeOf";
 
 const Validators = {
+  either,
   inEmailFormat,
+  inStringLength,
   isRequired,
+  oneOf,
   typeOf,
 };
 
